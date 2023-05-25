f80c06ba7b393e562a69f3a79ec644ffab181d0e
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-25 04:23:35 -0700

    Add tak 0

--- print_error.c
+++ print_error.c
@@ -14,7 +14,7 @@ void print_error(int error, unsigned int line_number, char *opcode,
 	switch (error)
 	{
 		case 1:
-			write(STDERR_FILENO, "USAGE: monty file\n", strlen("USAGE: monty file\n"));
+			fprintf(stderr, "USAGE: monty file\n");
 			break;
 		case 2:
 			fprintf(stderr, "Error: Can't open file %s\n", opcode);
